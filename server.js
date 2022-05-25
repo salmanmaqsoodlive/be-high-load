e67101c7a5c22e8d2a2f73c4f6f8ff3f77c9f8d4
Salman Maqsood <isalmanmaqsood@gmail.com>
2022-05-25 20:24:39 +0500

    fixed issues

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const serveStatic = require("serve-static");
 const path = require("path");
+const history = require("connect-history-api-fallback");
+// ^ middleware to redirect all URLs to index.html
 
 const app = express();
+const staticFileMiddleware = express.static(path.join(__dirname, "/dist"));
 
-app.use("/", serveStatic(path.join(__dirname, "/dist")));
+// app.use("/", serveStatic(path.join(__dirname, "/dist")));
+app.use(staticFileMiddleware);
+app.use(history());
+app.use(staticFileMiddleware);
 
+app.get("/", function (req, res) {
+  res.render(path.join(__dirname + "/index.html"));
+});
 const port = process.env.PORT || 8080;
 
 app.listen(port);
