ca708e729ec1f8dd90d385eb3bd6b26b42927f92
Salman Maqsood <isalmanmaqsood@gmail.com>
2022-05-25 20:20:21 +0500

    latest build

--- dist/js/about.fa48ec5a.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkbe_high_load"]=self["webpackChunkbe_high_load"]||[]).push([[443],{830:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var u=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],l=n(1),s={},r=(0,l.Z)(s,u,a,!1,null,null,null),i=r.exports}}]);
-//# sourceMappingURL=about.fa48ec5a.js.map
